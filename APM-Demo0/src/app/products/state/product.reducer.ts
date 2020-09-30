5bdcf32f9d8eefcaf6acae53d61b814f8820c382
Duncan Joyce <Duncan_Joyce@waters.com>
2020-09-30 14:13:13 +0100

    Fix state tree to maintain lazy loading boundaries

--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,5 +1,11 @@
 import { createAction, createReducer, on } from '@ngrx/store';
+import * as AppState from '../../state/app.state';
+
 import { Product } from '../product';
+
+export interface State extends AppState.State {
+  products: ProductState;
+}
 
 export interface ProductState {
   showProductCode: boolean;
